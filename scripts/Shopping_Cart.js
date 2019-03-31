f010d4b901b0109e82cd01b977a0a0a18542db51
Oriyn <gabehagg@gmail.com>
2019-03-31 11:18:37 -0500

    progress on shopping cart

--- scripts/Shopping_Cart.js
+++ scripts/Shopping_Cart.js
@@ -1,8 +1,28 @@
+
 function addToCart (itemNo){
-var wholeOrder = "";
+localStorage.setItem("NumOfItems", 0);    
+addItem();
 var APIcall = cartAPI(itemNo);
+var itemsInCart = [];
+itemsInCart[localStorage.getItem("NumOfItems")] = APIcall;
+localStorage.setItem("CartItems", JSON.stringify(itemsInCart));
+var storedNames = JSON.parse(localStorage.getItem("CartItems"));
+var cartCount = storedNames.length;
+
+alert(cartCount);
+
 alert(APIcall);  
 };
+
+function addItem(){
+    var itemCount = parseInt(document.getElementById("cartNum").innerHTML);
+    itemCount += 1;
+    document.getElementById("cartNum").innerHTML = itemCount;
+    alert(itemCount);
+};
+
+
+
 
 function listCookies(wholeOrder) {
     document.cookie = 'Shopping cart = ' + wholeOrder;
