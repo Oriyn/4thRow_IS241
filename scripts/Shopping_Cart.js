b6d916d854776478961ab2d32e03715ddd67290b
Oriyn <gabehagg@gmail.com>
2019-04-01 15:23:48 -0500

    more progress on shopping cart

--- scripts/Shopping_Cart.js
+++ scripts/Shopping_Cart.js
@@ -1,38 +1,22 @@
+window.onload = function (){
+   localStorage.setItem("NumOfItems", 0); 
+};
+
 
 function addToCart (itemNo){
-localStorage.setItem("NumOfItems", 0);    
-addItem();
+var numOfItems = localStorage.getItem("NumOfItems");
+alert(numOfItems);   
 var APIcall = cartAPI(itemNo);
-var itemsInCart = [];
-itemsInCart[localStorage.getItem("NumOfItems")] = APIcall;
+itemsInCart[numOfItems] = APIcall;
 localStorage.setItem("CartItems", JSON.stringify(itemsInCart));
-var storedNames = JSON.parse(localStorage.getItem("CartItems"));
-var cartCount = storedNames.length;
-
-alert(cartCount);
-
+numOfItems ++;
+localStorage.setItem("NumOfItems", numOfItems);
 alert(APIcall);  
+addItem();
+var storedNames = JSON.parse(localStorage.getItem("CartItems"));
+console.log(storedNames);
 };
 
 function addItem(){
-    var itemCount = parseInt(document.getElementById("cartNum").innerHTML);
-    itemCount += 1;
-    document.getElementById("cartNum").innerHTML = itemCount;
-    alert(itemCount);
-};
-
-
-
-
-function listCookies(wholeOrder) {
-    document.cookie = 'Shopping cart = ' + wholeOrder;
-    var theCookies = document.cookie.split(';');
-    alert(theCookies);
-    var aString = '';
-    for (var i = 1 ; i <= theCookies.length; i++) {
-        aString += i + ' ' + theCookies[i-1] + "\n";
-    }
-    alert("hit listcookies");
-    alert(wholeOrder)
-    alert(aString);
+    document.getElementById("cartNum").innerHTML = localStorage.getItem("NumOfItems") ;
 };
