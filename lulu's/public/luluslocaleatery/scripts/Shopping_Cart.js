637971461e920e3b30ba1f9af320e82ad50c4e45
Oriyn <gabehagg@gmail.com>
2019-04-25 13:51:48 -0500

    blew up the world so i can fix it again

---
+++ lulu's/public/luluslocaleatery/scripts/Shopping_Cart.js
@@ -0,0 +1,40 @@
+window.onload = function (){
+    if(localStorage.getItem("NumOfItems") === null) {
+        document.getElementById("cartNum").innerHTML = "0";
+    } else {
+    document.getElementById("cartNum").innerHTML = localStorage.getItem("NumOfItems");
+    }
+};
+
+
+function addToCart (itemNo){   
+ if (document.getElementById("cartNum").innerHTML === "0")
+ {
+    var itemsInCart = [];
+    localStorage.setItem("NumOfItems", 0);
+ }
+ else
+ {
+     var itemsInCart = JSON.parse(localStorage.getItem("CartItems"));
+ }
+var numOfItems = localStorage.getItem("NumOfItems");  
+var APIcall = cartAPI(itemNo);
+itemsInCart[numOfItems] = APIcall;
+localStorage.setItem("CartItems", JSON.stringify(itemsInCart));
+numOfItems ++;
+localStorage.setItem("NumOfItems", numOfItems);
+addItem();
+var storedNames = JSON.parse(localStorage.getItem("CartItems"));
+console.log(storedNames);
+};
+
+function addItem(){
+    document.getElementById("cartNum").innerHTML = localStorage.getItem("NumOfItems") ;
+};
+
+function clearCart(){
+  localStorage.clear();
+  document.getElementById("cartNum").innerHTML = "0";
+  alert("cart cleared");
+  window.location.reload();
+};
